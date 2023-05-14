c27668a938639550803454c7c1179335bc75a327
Madalena Silva Costa Giao Morais <msilva-c@c1r13s2.42lisboa.com>
2023-05-14 18:24:19 +0100

    libft

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,15 +6,15 @@
 /*   By: msilva-c <msilva-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 19:49:08 by msilva-c          #+#    #+#             */
-/*   Updated: 2023/05/12 16:12:15 by msilva-c         ###   ########.fr       */
+/*   Updated: 2023/05/14 18:20:45 by msilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstiter(t_list *lst, void (*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-    t_list	*temp;
+	t_list	*temp;
 
 	temp = lst;
 	while (temp != NULL)
